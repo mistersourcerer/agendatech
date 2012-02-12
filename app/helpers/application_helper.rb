1c79ab389fd4e53df076826038e5eb3e1b3061a0
Alberto Souza <alots.ssa@gmail.com>
2012-02-12 18:47:33 -0200

    pegando ultimos comentarios direto do facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,11 @@ module ApplicationHelper
   def facebook_like_button
      raw %&<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like-box href="http://www.facebook.com/pages/Agendatech/190166907684709" width="230" show_faces="true" stream="true" header="true"></fb:like-box>&
   end
+
+  def fb_urls_dos_eventos
+	 urls = Evento.para_o_ano(Time.now.year).collect{|evento| %Q{"#{evento_full_url(evento)}"} }
+	 raw(urls.join(","))
+  end
 end
 
 
