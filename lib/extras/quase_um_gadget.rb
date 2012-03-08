408ffd70e35dd49a0d4a7822e89c58f876649274
Alberto Souza <alots.ssa@gmail.com>
2012-03-08 11:39:26 -0300

    corrigindo bug do agendatech com gadget

--- lib/extras/quase_um_gadget.rb
+++ lib/extras/quase_um_gadget.rb
@@ -5,10 +5,11 @@ class QuaseUmGadget
   end
   
   def associa(user)    
-    if user.tem_gadget? Evento.find(@evento_id),@tipo
-      gadget = user.gadget_para @evento,@tipo
+    evento = Evento.find(@evento_id)
+    if user.tem_gadget? evento,@tipo
+      gadget = user.gadget_para evento,@tipo
     else
-      gadget = Gadget.create :tipo => @tipo, :evento_id => @evento_id, :user_id => user.id
+      gadget = Gadget.create :tipo => @tipo, :evento_id => evento.id, :user_id => user.id
     end
     gadget
   end
