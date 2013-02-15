7bf56d813e2a596fc6d0196f260afdcceaa893de
Joviane Jardim <joviane@gmail.com>
2013-02-15 15:10:19 -0200

    Extraindo variaveis de ambiente para uso no omniauth e no twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,13 @@
+consumer_key = ENV['TWITTER_CONSUMER_KEY']
+consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  provider :twitter, consumer_key, consumer_secret
 end 
 
 Twitter.configure do |config|
-  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.consumer_key = consumer_key
+  config.consumer_secret = consumer_secret
   config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
   config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
 end
