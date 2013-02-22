79b2c1c4f74537f90b474f2c06fa80f3ab5fed79
Joviane Jardim <joviane@gmail.com>
2013-02-21 21:23:29 -0300

    [Rails 4] melhorando o update do contexto das tags

--- db/migrate/20130221233503_add_context_tagger_to_taggings.rb
+++ db/migrate/20130221233503_add_context_tagger_to_taggings.rb
@@ -2,8 +2,6 @@ class AddContextTaggerToTaggings < ActiveRecord::Migration
   def change
     add_reference :taggings, :tagger, index: true, :polymorphic => true
     add_column :taggings, :context, :string
-    ActsAsTaggableOn::Tagging.all.each do |tagging|
-      tagging.update_attributes! :context => "tags"
-    end
+    ActsAsTaggableOn::Tagging.update_all(context: "tags")
   end
 end
